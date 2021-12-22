e09a7d9b1134f1462f9643c326591ee9c79a3f02
javidanwari <javidanwari@10.0.0.11>
2021-12-21 19:31:04 -0500

    Initial Commit

---
+++ src/test/java/com/noorteck/java/hw6/day6Demo.java
@@ -0,0 +1,25 @@
+package com.noorteck.java.hw6;
+
+public class day6Demo {
+
+	public static void main(String[]args) {
+		
+		System.out.println("Test 101....");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
